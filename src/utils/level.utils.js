6a8d894690bb0936012863d7a701fe71b100bb55
Jorge Naventa <jmnch0@gmail.com>
2024-11-14 12:33:08 -0500

    feat: primitive calculate points

--- src/utils/level.utils.js
+++ src/utils/level.utils.js
@@ -3,50 +3,6 @@ const levelConsts = {
   distanceInHue: [20, 30, 40, 50, 60, 70],
   distanceMonochrome: [5, 4, 3, 2, 1],
 }
-/*
-export function generatePath(fields) {
-  const coords = {}
-  const savedPosition = {}
-
-  function getNewPosition(x, y) {
-    const flow = Math.floor(Math.random() * 2)
-    const dir = Math.floor(Math.random() * 2)
-
-    const apply = flow > 0 ? -1 : 1
-    const newPosition = dir === 0 ? [x + apply, y] : [x, y + apply]
-
-    return savedPosition[String(newPosition)] ? getNewPosition(x, y) : newPosition
-  }
-
-  for (let a = 0; a < fields; a++) {
-    if (a === 0) {
-      coords[a] = [a, a]
-      savedPosition[`${a}, ${a}`] = true
-    } else {
-      const [lastX, lastY] = coords[a - 1] //obtener Posición anterior
-      const [newX, newY] = getNewPosition(lastX, lastY)
-      coords[a] = [newX, newY]
-      savedPosition[`${newX}, ${newY}`] = true
-    }
-  }
-
-  return {
-    path: Object.values(coords),
-    savedPosition,
-  }
-}
-
-function addBlock(point, lastDir) {
-  let possibleDir = Math.floor(Math.random() * 4)
-  const d = {
-    0: point => [point[0] - 1, point[1]],
-    1: point => [point[0], point[1] + 1],
-    2: point => [point[0] + 1, point[1]],
-    3: point => [point[0], point[1] - 1],
-  }
-
-  return { dir: d[possibleDir](point), lastDir: possibleDir }
-}*/
 
 function generatePath(spaces) {
   const savedPosition = {}
@@ -81,31 +37,6 @@ function generatePath(spaces) {
     savedPosition,
   }
 }
-
-/*function generatePath(dots, possiblePath = 4) {
-  const coords = {}
-  const savedPosition = {}
-
-  const possibleStart = Math.floor(Math.random() * 4)
-
-  if (possiblePath === 4) {
-    coords[0] = [0, 0]
-    savedPosition['0, 0'] = true
-  }
-
-  let lastDirection = null
-  for (let i = 1; i < dots; i++) {
-    const { dir, lastDir } = addBlock(coords[i - 1], lastDirection)
-    lastDirection = lastDir
-    coords[i] = dir
-    savedPosition[`${coords[i][0]}, ${coords[i][1]}`] = true
-  }
-
-  return {
-    path: coords,
-    savedPosition,
-  }
-}*/
 
 function calculateMax(coords, xy = 0) {
   return coords.reduce(
@@ -143,4 +74,16 @@ export function generateField(steps) {
     path,
     gridMap,
   }
+}
+
+export function calculatePoints({ level, time, steps }) {
+  const levelStart = 1000
+  const timePenalization = 10
+  const movePenalization = 10
+  const minMovements = 4
+  const additionalMoves = steps - minMovements
+
+  const points = levelStart / (1 + time / timePenalization) - additionalMoves * movePenalization
+  console.log('points::', points)
+  return points
 }
