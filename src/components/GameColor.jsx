736ccf7d3974249bb41c5c4d9d5c83574c7f441b
Jorge Naventa <jmnch0@gmail.com>
2024-11-14 12:58:03 -0500

    feat: add check indicator for color item

--- src/components/GameColor.jsx
+++ src/components/GameColor.jsx
@@ -1,7 +1,8 @@
 import { useDraggable } from '@dnd-kit/core'
 import { CSS } from '@dnd-kit/utilities'
+import { Icon } from '@iconify/react/dist/iconify.js'
 
-export function GameColor({ id, children, color, data }) {
+export function GameColor({ id, children, color, data, isInCorrectPlace = false }) {
   const { attributes, listeners, setNodeRef, transform } = useDraggable({
     id: id,
     data,
@@ -16,13 +17,22 @@ export function GameColor({ id, children, color, data }) {
     backgroundColor: color,
     textShadow: '1px 1px 2px #000',
     display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+    boxSizing: 'border-box',
+    padding: '0.5em',
     // Outputs `translate3d(x, y, 0)`
     transform: CSS.Translate.toString(transform),
   }
 
-  return (
+  return isInCorrectPlace ? (
+    <div style={style}>
+      {children}
+      <div style={{ background: 'rgba(0,0,0,0.5)', borderRadius: '50%', width: '1em', height: '1em', display: 'flex' }}>
+        <Icon icon="mynaui:check" />
+      </div>
+    </div>
+  ) : (
     <div ref={setNodeRef} style={style} {...listeners} {...attributes}>
       {children}
     </div>
